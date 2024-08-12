4f72a6aa8ee23e327d4b2836e97717c1e2203316
Sergei Abrashkin <sergey.abrashkin@x5.ru>
2024-08-12 18:41:49 +0300

    add role check for users tab

--- app/cabinet/@tabs/users/page.tsx
+++ app/cabinet/@tabs/users/page.tsx
@@ -1,4 +1,14 @@
-const CabinetTabUsers = () => {
+import { userGet } from '@/lib/api-user'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+const CabinetTabUsers = async () => {
+  const userId = cookies().get('userId')?.value
+
+  const user = await userGet({ userId: Number(userId) })
+
+  if (!user.isAdmin) redirect('/cabinet')
+
   return <div className="">Users tab</div>
 }
 
