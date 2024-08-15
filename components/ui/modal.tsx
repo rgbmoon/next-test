9bb728bbab7ef8c3258d6a34ebc06179c5eeb589
Sergei Abrashkin <sergey.abrashkin@x5.ru>
2024-08-15 23:33:48 +0300

    fixed modal component

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,17 +1,25 @@
 'use client'
 
-import { FC, PropsWithChildren } from 'react'
+import { FC, PropsWithChildren, useEffect } from 'react'
 import { CloseIcon } from '../icons/close'
 import { useRouter } from 'next/navigation'
+import { createPortal } from 'react-dom'
 
 export const Modal: FC<PropsWithChildren> = ({ children }) => {
   const router = useRouter()
 
-  // TODO: disable scroll under modal when open
+  // this workaround needed to prevent body scroll when the modal is rendered
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden', 'h-auto')
 
-  return (
-    <div className="absolute left-0 top-0 z-[999] flex h-[100vh] w-[100vw]">
-      <div className="max-md-w-full z-[9999] m-auto flex min-h-[12rem] w-[40rem] flex-col gap-2 rounded-lg border border-solid border-slate-100 bg-white p-3 shadow-lg max-md:m-2">
+    return () => {
+      document.body.classList.remove('overflow-hidden', 'h-auto')
+    }
+  }, [])
+
+  return createPortal(
+    <div className="fixed bottom-0 left-0 right-0 top-0 z-[999] flex bg-black bg-opacity-25 p-2">
+      <div className="max-md-w-full z-[9999] m-auto flex min-h-[12rem] w-[40rem] flex-col gap-2 rounded-lg border border-solid border-slate-100 bg-white p-3 shadow-lg">
         <div className="flex justify-end">
           <button
             onClick={() => {
@@ -23,7 +31,7 @@ export const Modal: FC<PropsWithChildren> = ({ children }) => {
         </div>
         <div className="">{children}</div>
       </div>
-      <div className="absolute left-0 top-0 z-[999] flex h-[100vh] w-[100vw] bg-black bg-opacity-25"></div>
-    </div>
+    </div>,
+    document.body
   )
 }
